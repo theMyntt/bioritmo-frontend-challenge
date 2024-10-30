54fcc2774fa1e8272f176d4fe836a381e2510af2
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-30 20:39:19 -0300

    feat: only home page component

---
+++ src/app/features/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TLocationDTO } from '../../types/locaction';
+import { Store } from '@ngrx/store';
+import { selectLocationState } from '../../store/selectors/locations.selector';
+import { AppState } from '../../store/reducers/locations.reducers';
+import { fetchData } from '../../store/actions/location.actions';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent implements OnInit {
+  protected readonly locations$!: Observable<TLocationDTO>
+
+  constructor(private store: Store<AppState>) {
+    this.locations$ = this.store.select(selectLocationState)
+  }
+
+  ngOnInit(): void {
+    this.fetchLocations()
+  }
+
+  protected fetchLocations() {
+    this.store.dispatch(fetchData())
+  }
+}
