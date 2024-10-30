c028e8f76457c6e04892d67b96e99474994f77f5
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-30 18:55:28 -0300

    chore: types of response

---
+++ src/app/types/locaction.ts
@@ -0,0 +1,22 @@
+export type TLocationDTO = {
+  current_country_id: number
+  locations: Array<TLocation>
+  wp_total: number
+  total: number
+  success: boolean
+}
+
+export type TLocation = {
+  id: number
+  title: string
+  content: string
+  opened: boolean
+  mask: string
+  towel:string
+  fountain: string
+  locker_room: string
+  schedule: Array<{
+    weekdays: string
+    hour: string
+  }>
+}
