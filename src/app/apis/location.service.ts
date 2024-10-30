2c7c6b4c07e18e116182b4f4ad619b261bb18f46
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-30 20:38:13 -0300

    chore: return an http client

--- src/app/apis/location.service.ts
+++ src/app/apis/location.service.ts
@@ -1,13 +1,15 @@
-import { Injectable } from "@angular/core";
-import { of } from "rxjs";
-import { TLocationDTO } from "../types/locaction";
+import { Injectable } from '@angular/core';
+import { from, Observable } from 'rxjs';
+import { TLocationDTO } from '../types/locaction';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LocationService {
-  async fetchLocations() {
-    const response = await fetch('/locations.json')
-    return of(response.json() as unknown as TLocationDTO)
+  constructor(private http: HttpClient) {}
+
+  fetchLocations() {
+    return this.http.get<TLocationDTO>('/locations.json');
   }
 }
