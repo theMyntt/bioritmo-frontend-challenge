42a349fa7efd0b8ef397d8fa88d35c68e9d77293
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-11-02 16:16:01 -0300

    chore: picking total gyms  on response

--- src/app/features/home/components/time-picker/time-picker.component.ts
+++ src/app/features/home/components/time-picker/time-picker.component.ts
@@ -1,21 +1,44 @@
-import { Component, signal } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, OnInit, signal } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../../../store/reducers/locations.reducers';
+import { selectLocationState } from '../../../../store/selectors/locations.selector';
+import { Observable } from 'rxjs';
+import { TLocationDTO } from '../../../../types/locaction';
+import { CommonModule } from '@angular/common';
+import { fetchData } from '../../../../store/actions/location.actions';
 
 @Component({
   selector: 'app-time-picker',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './time-picker.component.html',
-  styleUrl: './time-picker.component.scss'
+  styleUrl: './time-picker.component.scss',
 })
-export class TimePickerComponent {
-  itemsCount = signal(0)
-  timeForm = new FormGroup({
+export class TimePickerComponent implements OnInit {
+  protected itemsCount = signal(0);
+  protected readonly locations$!: Observable<TLocationDTO>;
+  protected timeForm = new FormGroup({
     time: new FormControl('', [Validators.required]),
-    closedGyms: new FormControl(false)
-  })
+    closedGyms: new FormControl(false),
+  });
 
-  submit() {
-    console.log(this.timeForm)
+  public submit() {
+    console.log(this.timeForm);
+  }
+
+  constructor(private store: Store<AppState>) {
+    this.locations$ = this.store.select(selectLocationState);
+  }
+
+  ngOnInit(): void {
+    this.locations$.subscribe((data: any) => {
+      this.itemsCount.set(data.locations.total); 
+    });
   }
 }
