544ddb9d1849523010e17a7f88d06cb0edd6bf49
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-11-02 16:32:26 -0300

    chore: query form actions

--- src/app/store/actions/location.actions.ts
+++ src/app/store/actions/location.actions.ts
@@ -1,8 +1,18 @@
 import { createAction, props } from '@ngrx/store';
 import { TLocationDTO } from '../../types/locaction';
+import { IQueryForm } from '../../types/queryForm';
 
 export const fetchData = createAction('[Location] Fetch Data');
 export const fetchDataSuccess = createAction(
   '[Location] Fetch Data Success',
   props<{ locations: TLocationDTO }>()
+);
+
+export const queryForm = createAction(
+  '[Location] Query form',
+  props<{ form: IQueryForm }>()
+);
+export const queryFormSuccess = createAction(
+  '[Location] Query form Success',
+  props<{ form: IQueryForm }>()
 );
