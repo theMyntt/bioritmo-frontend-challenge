3223444f5952ab151a6cfd6661acb20ee16660fb
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-11-02 15:58:53 -0300

    chore: implementation of reactive forms

--- src/app/features/home/components/time-picker/time-picker.component.ts
+++ src/app/features/home/components/time-picker/time-picker.component.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-time-picker',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './time-picker.component.html',
   styleUrl: './time-picker.component.scss'
 })
 export class TimePickerComponent {
   itemsCount = signal(0)
+  timeForm = new FormGroup({
+    time: new FormControl('', [Validators.required]),
+    closedGyms: new FormControl(false)
+  })
+
+  submit() {
+    console.log(this.timeForm)
+  }
 }
